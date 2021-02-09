e0dfc749c80694cd960193dd800ed3a558fe1256
Ahmed Ali <pirates0159@gmail.com>
2021-02-09 17:42:24 +0600

    new update

--- app.js
+++ app.js
@@ -8,43 +8,44 @@ const searchMeals = () => {
 
 const displayMeals = meals => {
     const mealsDiv = document.getElementById('meals');
+    mealsDiv.innerHTML = '';
     meals.forEach(food => {
-
-  //  for (let i = 0; i < meals.length; i++) {
-        //const food = meals[i];
         const mealDiv = document.createElement('div');
-        
-        // const h3 = document.createElement('h3');
-        // h3.innerText = food.strMeal;
-        // const img = document.createElement('img');
-        // img.innerText = food.strMealThumb;
-        // mealDiv.appendChild(h3);
-        // mealDiv.appendChild(img);
         mealDiv.className = `food`;
         const mealInfo = `
+        <img style="width:200px; border-radius:20px;"src="${food.strMealThumb}">
         <h5 class ="food-strMeal">${food.strMeal}</h5>
-        <button onclick="displayFoodDetails('${food.strMeal}')">details</button>
+        <button class="btn btn-primary" onclick="displayMealsDetail('${food.idMeal}')">Ingredient</button>
         `;
         mealDiv.innerHTML = mealInfo;
         mealsDiv.appendChild(mealDiv);
     });
-
-      // console.log(food.strMeal);
-   // }
 }
 
-       displayFoodDetails = list => {
-       const url = `https://www.themealdb.com/api/json/v1/1/categories.php?s=${list}`    
-       fetch(url)
-       .then(res => res.json())
-       .then(data => renderFoodInfo(data.meals));
-       
-}
+const displayMealsDetail = list => {
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${list}`
+  fetch(url)
+  .then(res => res.json())
+  .then(data => displayItemDetails(data.meals[0]));  
 
-        const renderFoodInfo = food => {
-        console.log(food);
-      const mealDiv = document.getElementById('mealsDetail');
-      mealDiv .innerHTML = `
+}
+const displayItemDetails = food => {
+  const Ingredients = document.getElementById('mealsDetail');
+  Ingredients.innerHTML = `
+  <img style="width:300px; margin-bottom:20px;  border-radius:10px;" src="${food.strMealThumb}">
+  <ul class ="food-ingredient">
+      <li><h3>Ingredient<h3></li>
+      <li><p>${food.strIngredient1}</p></li>
+      <li><p>${food.strIngredient2}</p></li>
+      <li><p>${food.strIngredient3}</p></li>
+      <li><p>${food.strIngredient4}</p></li>
+      <li><p>${food.strIngredient5}</p></li>
+      <li><p>${food.strIngredient6}</p></li>
+      <li><p>${food.strIngredient7}</p></li>
+      <li><p>${food.strIngredient8}</p></li>
+      <li><p>${food.strIngredient10}</p></li>
+  <ul>  
+  `
+} 
+
       
-      `  
-     }
